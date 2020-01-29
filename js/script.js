177ef0c68393246e04caed419ee91fd104ead766
indusgeeks-developer <60337429+indusgeeks-developer@users.noreply.github.com>
2020-01-29 11:34:06 +0530

    Home Page

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Resize your browser for effect
+$('.slider').slick({
+  infinite: true,
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	fade: true,
+	fadeSpeed: 1000
+});
