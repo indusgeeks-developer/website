c4da1a1411d0bee690d6cd448e83e2c7d6f1072b
indusgeeks-developer <60337429+indusgeeks-developer@users.noreply.github.com>
2020-01-30 16:20:25 +0530

    Home Page - css modifications II

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 // Resize your browser for effect
-$('.slider').slick({
-  infinite: true,
-	dots: true,
-	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	fade: true,
-	fadeSpeed: 1000
+$(document).ready(function(){
+   $('.slider-hero').slick({
+	  	infinite: true,
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		fade: true,
+		fadeSpeed: 1000
+	});
+
+	$('.slider-testimonials').slick({
+		infinite: true,
+		dots: false,
+		prevArrow: $(".prev"),
+    	nextArrow: $(".next"),
+		fade: true,
+		fadeSpeed: 1000
+	});
 });
