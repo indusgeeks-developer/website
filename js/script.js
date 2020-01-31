3ee6fa029c4bcc9f50b190b792afe4a274a0b73b
indusgeeks-developer <60337429+indusgeeks-developer@users.noreply.github.com>
2020-01-31 11:26:45 +0530

    Home page - mobile navbar logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Resize your browser for effect
 $(document).ready(function(){
+
+	$('input[name="nav-check"]').click(function(){
+	    if($(this).prop("checked") == true){
+	        $(".mobile-body-overly").css({"display":"block"});
+	        $('input[name="drop-check"]').prop("checked", false);
+	    }
+	    else if($(this).prop("checked") == false){
+	        $(".mobile-body-overly").css({"display":"none"});
+	    }
+    });
+
    $('.slider-hero').slick({
 	  	infinite: true,
 		dots: true,
